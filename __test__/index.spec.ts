02fa82581f2c516711552444f4cb3dec60339399
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-11 10:48:17 +0900

    change: demo code.

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,13 +1,43 @@
-import { APIGatewayProxyEvent } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 import { handler } from '../src'
 
 describe('handler()', () => {
+  beforeEach(() => {
+    // set environments
+    process.env.BUCKET = undefined
+
+    // mock aws-sdk
+    jest
+      .spyOn((AWS.S3 as any).services['2006-03-01'].prototype, 'getObject')
+      .mockImplementation(() => ({
+        promise: (): any => ({
+          $response: {
+            httpResponse: {
+              statusCode: 200,
+            },
+          },
+          Body: undefined,
+        }),
+      }))
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
   it('returns dummy response', async () => {
     expect.assertions(1)
 
-    return expect(handler({} as APIGatewayProxyEvent)).resolves.toEqual({
+    // set environments
+    process.env.BUCKET = 'TARGET_BUCKET_NAME'
+
+    return expect(
+      handler({
+        pathParameters: { key: 'image.jpg' },
+      } as any)
+    ).resolves.toEqual({
       statusCode: 200,
-      body: '',
+      body: JSON.stringify({ key: 'image.jpg' }),
     })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+const s3 = new AWS.S3()
 
 export async function handler(
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> {
-  console.log('hoge', event)
+  try {
+    const Bucket = process.env.BUCKET
 
-  return {
-    statusCode: 200,
-    body: '',
+    if (Bucket === undefined) {
+      throw new Error('Bucket name is required')
+    }
+
+    if (!event.pathParameters?.key) {
+      throw new Error('Object key is required')
+    }
+
+    const { key } = event.pathParameters
+    const res = await s3
+      .getObject({
+        Bucket,
+        Key: key,
+      })
+      .promise()
+
+    console.log('hoge', res)
+
+    return {
+      statusCode: res.$response.httpResponse.statusCode,
+      body: JSON.stringify({ key }),
+    }
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 400,
+      body: JSON.stringify(err.message),
+    }
   }
 }
