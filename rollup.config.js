3ad0d3613fe18672f5ce5ae8be69360e282b50d7
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-11 22:04:23 +0900

    chore: optimize dpendencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
 import builtins from 'builtin-modules'
 import commonjs from 'rollup-plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
-import json from 'rollup-plugin-json'
-import resolve from 'rollup-plugin-node-resolve'
 import progress from 'rollup-plugin-progress'
 import typescript from 'rollup-plugin-typescript2'
 
